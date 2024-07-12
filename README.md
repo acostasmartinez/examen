95709f79722dd9de15217c9164b6ba21cfa9728f
ACM <108570690+acostasmartinez@users.noreply.github.com>
2024-07-12 09:55:31 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# examen
+# examenmod3
---
+++ codigo.js
@@ -0,0 +1,20 @@
+let fotosMarcadas = 0;
+
+function marcarFoto(evt) {
+    if(evt.target.dataset.marcada == undefined) {
+        evt.target.style.filter = "sepia()";
+        evt.target.dataset.marcada = true;
+        fotosMarcadas++;
+    } else {
+        evt.target.style.filter = "";
+        evt.target.dataset.marcada = false;
+        fotosMarcadas--;
+    }
+    document.getElementById("marcadas").textContent = fotosMarcadas;
+}
+
+const fotos = document.querySelectorAll("#fotos img")
+for (const foto of fotos) {
+    foto.addEventListener("click", marcarFoto);
+}
+
